80499be46c95dabcd27ad0cb8da87aebd004417c
Daniel <jomafica13.dp@gmail.com>
2020-09-30 16:41:53 +0100

    update

--- Inlfuxdb.py
+++ Inlfuxdb.py
@@ -2,40 +2,54 @@ import requests, json, time
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-
-def iterate_dict(values):
+def iteratev2(values):
     json_body = []
-    for d in jparsed_dic:
-        if isinstance(d,dict):
-            for keyz, valuez in d.items():
-                tags = {}
-                if isinstance(valuez, dict):
-                    for keyy, valuey in valuez.items():
-                        json_line = {}
-                        if isinstance(valuey, dict):
-                            json_line["measurement"] = keyy
-                            fields = {}
-                            for keyx, valuex in valuey.items():
-                                fields[keyx] = valuex
-                            json_line['tags'] = tags
-                            json_line['fields'] = fields
-                            json_body.append(json_line)
-                        else:
-                            pass
-                            #print(keyy + ":" + str(valuey))
-                else:
-                    tags[keyz] = valuez
-                    #print(keyz + ":" + str(valuez))
+    tags = {}
+    for key , value in values.items():
+        if not isinstance(value, dict):
+            tags[key] = value
+
+
+    for keyr , valuer in values['results'].items():
+        #print(keyr)
+        if isinstance(values['results'][keyr], dict):
+            json_line = {}
+            json_line['measurement'] = keyr
+            json_line['tags'] = tags
+            fields = {}
+            for keyf , valuef in valuer.items():
+                fields[keyf] = valuef
+            json_line['fields'] = fields
+            #print(json_line)
+            json_body.append(json_line)
+        else:
+            json_line = {}
+            json_line['measurement'] = keyr
+            json_line['tags'] = tags
+            json_line['fields'] = {keyr:valuer}
+            #print(json_line)
+            json_body.append(json_line)
     return json_body
 
-def main():
-    json_body = iterate_dict(jparsed_dic)
-    print(json_body)
+
+def write__to_influx(json_body):
     for x in json_body:
-        #print(x)
+        print(x)
         write_api = client.write_api(write_options=SYNCHRONOUS)
         data = x
         write_api.write(bucket, org, data)
+
+def main():
+    json_body = {}
+    if isinstance(jparsed_dic, list):
+        for x in jparsed_dic:
+           json_body = iteratev2(x)
+           write__to_influx(json_body)
+    else:
+        json_body = iteratev2(jparsed_dic)
+        write__to_influx(json_body)
+    #print(json_body)
+
 
 
 if __name__ == "__main__":
@@ -55,8 +69,8 @@ if __name__ == "__main__":
     jparsed_dic = json.loads(response.content)
 
     parsed_json = {}
-    main()
-    #while response.status_code == 200:
-#
-    #    time.sleep(5)
+
+    while response.status_code == 200:
+        main()
+        time.sleep(5)
 
