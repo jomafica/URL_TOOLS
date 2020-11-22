e20a141d2ee9be42bb5e0ac59ab5b37e4b017074
Daniel <jomafica13.dp@gmail.com>
2020-11-22 23:24:41 +0000

    update

--- scrap_content/.venv/scracp.py
+++ scrap_content/.venv/scracp.py
@@ -6,7 +6,8 @@ from urllib.parse import urlparse
 def data_collection(data):
 
     json_body_temp = []
-    json_body = []
+    json_body_content = []
+    json_body = {}
 
     for art in data:
 
@@ -14,7 +15,7 @@ def data_collection(data):
         json_underline = {}
 
         # regex para filtrar o conteudo
-        regex_id = re.search(r'(?<=(id\=\"))[^\"]*.*?', str(art))
+        regex_id = re.search(r'(?<=(id\=\"post\-))[^\"]*.*?', str(art))
         title = re.search(r'(?<=(title\=\"))[^\"]*.*?', str(art))
         url_art = art.find("a", attrs={"title" : re.compile('')})
         url = re.search(r'(?<=(href\=\"))[^\"]*.*?', str(url_art))
@@ -33,11 +34,13 @@ def data_collection(data):
 
         if src_img or src_img2:
             # Append id as a key
-            json_line["id_art"] = regex_id.group(0)
+            json_line["status"] = "ok"
             json_body_temp.append(json_line)
+            json_underline["id"] = regex_id.group(0)
+
 
             # Dic for content
-            json_underline["title_art"] = title.group(0)
+            json_underline["title"] = title.group(0)
 
             if descrip:
                 json_underline["description"] = descrip.group(0)
@@ -46,21 +49,23 @@ def data_collection(data):
             if descrip3:
                 json_underline["description"] = descrip3.group(0)
             json_underline["date"] = date.text
-            json_underline["url_art"] = url.group(0)
+            json_underline["url"] = url.group(0)
 
             if src_img:
-                json_underline["img_art"] = src_img.group(0)
+                json_underline["img"] = src_img.group(0)
             if src_img2:
-                json_underline["img_art"] = src_img2.group(0)
+                json_underline["img"] = src_img2.group(0)
 
-            # Append Dic above under "content"
-            json_line["content"] = json_underline
+            # Append Dict above under "content"
+
+            json_body_content.append(json_underline)
+            json_line["articles"] = json_body_content
             json_body_temp.append(json_line)
 
     #Remove duplicates s   
     for i in range(len(json_body_temp)): 
         if json_body_temp[i] not in json_body_temp[i + 1:]: 
-            json_body.append(json_body_temp[i]) 
+            json_body = (json_body_temp[i]) 
 
     return json_body
 
@@ -72,7 +77,7 @@ if __name__ == "__main__":
     id_page = int()
     url = urlparse("https://pplware.sapo.pt")
 
-    @app.route('/api/v1/resource/main', methods=['GET'])
+    @app.route('/api/v1/resource/1', methods=['GET'])
     def api_main():
         r = requests.get(url.geturl())
         parse = BeautifulSoup(r.content, 'html.parser')
